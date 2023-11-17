dc7f365668e611f550bf94746c906e839b6cc333
Mateus Schverz <mate_fs@hotmail.com>
2023-11-16 23:32:35 -0300

    Create reduce-example.js

---
+++ reduce-example.js
@@ -0,0 +1,24 @@
+var array = [1,2,3,4,5]
+
+var result = array.reduce((accumulator,item) => accumulator+item);
+
+console.log(result) 
+
+
+
+// String example:
+
+const minhaString = "Reduce";
+var minhaStringLetras = minhaString.split('')
+const resultadoString = minhaStringLetras.reduce(concatenar, '');
+
+// Função que concatena dois caracteres
+function concatenar(acumulador, caractere) {
+    console.log(`${acumulador}${caractere}`)
+    return `${acumulador}${caractere}`;
+}
+
+// String de exemplo
+console.log(resultadoString);  // Saída: "Reduce"
+
+

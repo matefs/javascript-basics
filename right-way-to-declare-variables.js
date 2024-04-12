771bd445c979000c1fb557e7852fe9b40329e7a5
Mateus Schverz <matefs8569@gmail.com>
2024-04-12 09:05:38 -0300

    Create right-way-to-declare-variables.js

---
+++ right-way-to-declare-variables.js
@@ -0,0 +1,13 @@
+var name,
+    address,
+    city,
+    postalCode,
+    isAdmin = false;
+
+
+name = 'Matthew'
+console.log(name)
+
+console.log(`Is admin ? ${isAdmin}`)
+
+// https://github.com/rwaldron/idiomatic.js/
